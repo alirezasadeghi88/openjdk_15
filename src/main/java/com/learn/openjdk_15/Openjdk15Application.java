40686a03dd786fbb6ef6392463c39d9087226606
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-14 10:45:39 -0800

    S

---
+++ src/main/java/com/learn/openjdk_15/Openjdk15Application.java
@@ -0,0 +1,13 @@
+package com.learn.openjdk_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openjdk15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Openjdk15Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/openjdk_15/Openjdk15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.openjdk_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Openjdk15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
