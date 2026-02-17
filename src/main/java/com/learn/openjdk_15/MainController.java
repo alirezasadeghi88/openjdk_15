5a00e73d194604afdf5569236f259e0779c711e5
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-17 04:56:13 -0800

    add Product

--- src/main/java/com/learn/openjdk_15/MainController.java
+++ src/main/java/com/learn/openjdk_15/MainController.java
@@ -5,17 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-public class MainController {
-    @RequestMapping("/home")
-    public String home(
-            @RequestParam String color,
-            Model page) {
-        page.addText("username");
-        page.addText("color");
-        return "home.html";
-    }
-}
+
 
 
 
