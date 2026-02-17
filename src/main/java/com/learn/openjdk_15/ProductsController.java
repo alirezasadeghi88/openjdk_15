c83dce2e6406503d00f44b7040959b397990cb2a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-17 05:02:02 -0800

    add ProductsController

--- src/main/java/com/learn/openjdk_15/ProductsController.java
+++ src/main/java/com/learn/openjdk_15/ProductsController.java
@@ -2,7 +2,10 @@ package com.learn.openjdk_15;
 
 import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductsController {
@@ -10,10 +13,25 @@ public class ProductsController {
     public ProductsController(ProductService productService) {
         this.productService = productService;
     }
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public String viewProducts(Model model) {
         var products = productService.findAll();
         model.addText("products");
         return "products.html";
     }
+    @PostMapping("/products")
+    public String addProduct(
+            @RequestParam String name,
+            @RequestParam double price,
+            Model model
+    ) {
+        Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);
+        productService.addProduct(p);
+        var products = productService.findAll();
+        model.addText("products");
+        return "products.html";
+    }
 }
+
