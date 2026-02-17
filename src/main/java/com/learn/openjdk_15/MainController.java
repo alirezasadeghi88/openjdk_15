c6851058864668da81bca38f4c6a2ca8390912bc
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-17 04:53:23 -0800

    add ReplaceText

--- src/main/java/com/learn/openjdk_15/MainController.java
+++ src/main/java/com/learn/openjdk_15/MainController.java
@@ -1,15 +1,21 @@
 package com.learn.openjdk_15;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-
     @RequestMapping("/home")
-    public String home() {
+    public String home(
+            @RequestParam String color,
+            Model page) {
+        page.addText("username");
+        page.addText("color");
         return "home.html";
     }
 }
+
 
 
