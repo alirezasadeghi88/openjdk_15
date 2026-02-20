bbe273e9b9a7cb725c8f6f7931d33f7708cdc052
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-20 11:38:59 -0800

    c

---
+++ src/main/java/com/learn/openjdk_15/Hello.java
@@ -0,0 +1,7 @@
+package com.learn.openjdk_15;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
--- src/main/java/com/learn/openjdk_15/LoginController.java
+++ src/main/java/com/learn/openjdk_15/LoginController.java
@@ -1,7 +1,10 @@
 package com.learn.openjdk_15;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -10,4 +13,19 @@ public class LoginController {
         return "login.html";
     }
 
+    @PostMapping("/")
+    public String loginPost(
+            @RequestParam String username,
+            @RequestParam String password,
+            Model model
+    ) {
+        boolean loggedIn = false;
+        if (loggedIn) {
+            model.addText("message");
+        } else {
+            model.addText("message");
+        }
+        return "login.html";
+    }
 }
+
