c62d9a6f04bf1f244d787104327440f780016408
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-22 07:31:38 -0800

    add LoginController

--- src/main/java/com/learn/openjdk_15/LoginController.java
+++ src/main/java/com/learn/openjdk_15/LoginController.java
@@ -5,31 +5,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 @Controller
 public class LoginController {
-
-        @GetMapping("/")
-        public String loginGet() {
-            return "login.html";
-        }
-
+    // Omitted code
     @PostMapping("/")
     public String loginPost(
             @RequestParam String username,
             @RequestParam String password,
             Model model
     ) {
-        boolean loggedIn = false;
-        if (loggedIn) {
-            model.addText("message");
-        } else {
-            model.addText("message");
+        LoginProcessor loginProcessor = null;
+        loginProcessor.setUsername(username);
+        loginProcessor.setPassword(password);
+        boolean loggedIn = loginProcessor.login();
+        if (loggedIn) { 
+            return "redirect:/main";
         }
+        model.addText("message");
         return "login.html";
-
+    }
 }
 
-    }
 
 
