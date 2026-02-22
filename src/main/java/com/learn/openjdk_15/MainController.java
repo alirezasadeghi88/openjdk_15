6a12219ee1eee8f9a83dd5a142c2eaf36b5709b5
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-22 07:22:32 -0800

    add MainController

--- src/main/java/com/learn/openjdk_15/MainController.java
+++ src/main/java/com/learn/openjdk_15/MainController.java
@@ -2,9 +2,29 @@ package com.learn.openjdk_15;
 
 import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class MainController {
+    private final LoggedUserManagementService loggedUserManagementService;
+
+    public MainController(
+            LoggedUserManagementService loggedUserManagementService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+    }
+
+    @GetMapping("/main")
+    public String home() {
+        String username =
+                loggedUserManagementService.getUsername();
+        if (username == null) {
+            return "redirect:/";
+        }
+        return "main.html";
+    }
+}
 
 
 
